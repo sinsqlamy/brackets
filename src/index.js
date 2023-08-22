00cf16206c6b137b41f96b282542b6a3067534ab
Denis <ds19092004@gmail.com>
2023-08-22 18:02:25 +0200

    feat: do check func -(Tue, Aug 22, 2023 06:02:25 PM)-

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const openingBrackets = new Set();
+  const closingBrackets = new Set();
+  const matchingPairs = {};
+
+  for (const pair of bracketsConfig) {
+    openingBrackets.add(pair[0]);
+    closingBrackets.add(pair[1]);
+    matchingPairs[pair[1]] = pair[0];
+  }
+
+  for (const char of str) {
+    if (openingBrackets.has(char)) {
+      if (char === matchingPairs[char] && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (closingBrackets.has(char)) {
+      if (
+        stack.length === 0 ||
+        stack[stack.length - 1] !== matchingPairs[char]
+      ) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+
+  return stack.length === 0;
+};
